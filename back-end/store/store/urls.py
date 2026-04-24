aff8c46dd45bc51a98579269670c5653898490c3
Row <your_email@example.com>
2026-04-24 10:43:31 +0100

    Modify

--- back-end/store/store/urls.py
+++ back-end/store/store/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from django.conf import settingsgit 
+from django.conf import settings
 from django.conf.urls.static import static
 from super_mart import views 
 
