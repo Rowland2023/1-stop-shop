3fae7a5d3c101f34343387d375cb1365209a50ab
Row <your_email@example.com>
2026-04-25 04:50:28 +0100

    Modify views.py added authenticate function login

--- back-end/store/super_mart/views.py
+++ back-end/store/super_mart/views.py
@@ -4,6 +4,7 @@ from .serializers import ProductSerializer # You likely need this too
 from django.db import transaction
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth import authenticate, login
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import AllowAny
 from rest_framework import viewsets # <--- ADD THIS
